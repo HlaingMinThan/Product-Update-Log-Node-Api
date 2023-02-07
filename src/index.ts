449452b6e77bf315ca63610f6a29c7a06de3343f
HlaingMinThan <hlaingminthan@92@gmail.com>
2023-02-08 03:00:03 +0630

    dot env setup done

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import app from './server';
+import dotEnv from 'dotenv';
+dotEnv.config();
 
 // setup prisma with typescript reference -> https://www.prisma.io/docs/getting-started/quickstart
 // run migrations -> npx prisma migrate dev --name init -> this will craete migration & generate prisma client & install
@@ -6,3 +8,4 @@ import app from './server';
 app.listen(3001, () => {
     console.log('hello my app localhost:'+3001);
 })
+console.log(process.env.JWT_SECRET)
