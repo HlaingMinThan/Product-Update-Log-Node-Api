feda6c05aea1ee6632493e2ddae014b88b93d10a
HlaingMinThan <hlaingminthan@92@gmail.com>
2023-02-08 16:40:17 +0630

    add a prisma studio note

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ dotEnv.config();
 
 // setup prisma with typescript reference -> https://www.prisma.io/docs/getting-started/quickstart
 // run migrations -> npx prisma migrate dev --name init -> this will craete migration & generate prisma client & install
+//create gui prisma db -> npx prisma stuido
 
 app.listen(3001, () => {
     console.log('hello my app localhost:'+3001);
